1aa28cd59e28c4e129e4d64736dd5364cde51677
Sujay Kamatkar <sujaykamatkar382@gmail.com>
2023-03-25 21:30:24 +0530

    made some changes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,32 +2,45 @@ import ngo from "../assets/portfolio/ngo.png";
 import cryptoview from "../assets/portfolio/cryptoview.png";
 import FoodMap from "../assets/portfolio/FoodMap.png";
 import Rscomputers from "../assets/portfolio/Rscomputers.jpg";
+import jaybhavani from "../assets/portfolio/jaybhavani.png";
+import portfolio from "../assets/portfolio/portfolio.png";
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
       src: ngo,
+      desc: "A NGO Site for nog to manage the child labours and give them benefit of government schemes.",
       code: "https://github.com/kamatkarsujay/NGO-Site",
       demo: "https://ngositereact.netlify.app/",
     },
     {
       id: 2,
       src: Rscomputers,
+      desc: "An E-commerce platform that lets users buy computers parts and build their own PC",
       code: "https://github.com/kamatkarsujay/MERN-E-Com-Site",
       demo: "https://mern-rscomputers-store.herokuapp.com/",
     },
     {
       id: 3,
       src: cryptoview,
+      desc: "An one place to do the analysis of cryptocurrencies and see news about cryptocurrencies.",
       code: "https://github.com/kamatkarsujay/cryptoview",
       demo: "https://cryptoview-delta.vercel.app/",
     },
     {
       id: 4,
       src: FoodMap,
+      desc: "This is a mobile app, which lets you click a picture of a dish and gives you a detailed recipe using a ML model. ",
       code: "https://github.com/Maitreyi22/ImageToRecipe",
       demo: "https://github.com/Maitreyi22/ImageToRecipe",
+    },
+    {
+      id: 5,
+      src: portfolio,
+      desc: "I made this portfolio using reactjs and tailwind css and deployed with vercel.",
+      code: "https://github.com/kamatkarsujay/sujay-portfolio",
+      demo: "https://sujaykamatkar.vercel.app/",
     },
   ];
 
@@ -47,12 +60,13 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src, demo, code }) => (
+          {portfolios.map(({ id, src, demo, code, desc }) => (
             <div
               key={id}
               className="shadow-md rounded-lg cursor-pointer duration-200 hover:scale-105"
             >
               <img src={src} alt="" className="rounded-t-md	" />
+              <p className="px-4 pt-2">{desc}</p>
               <div className="flex items-center justify-center">
                 <a href={demo} target="_blank" className="w-1/2 px-6 py-3 m-4">
                   Demo
@@ -63,6 +77,22 @@ const Portfolio = () => {
               </div>
             </div>
           ))}
+          <div className="shadow-md rounded-lg cursor-pointer duration-200 hover:scale-105">
+            <img src={jaybhavani} alt="" className="rounded-t-md	" />
+            <p className="px-4 pt-2">
+              This was a freelancing project for an enterprise called Jaybhavani
+              Enterprises.
+            </p>
+            <div className="flex items-center justify-center">
+              <a
+                href="https://jaybhavani.in/"
+                target="_blank"
+                className="px-6 py-3 m-4"
+              >
+                Demo
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
